2a51471c8c69876d2c60c8db2c1311222b1134cc
Jorge Urquijo <jorgedzo_17@hotmail.com>
2019-06-07 15:10:46 -0700

    vistas basadas en clases

--- refugio_app/apps/mascota/urls.py
+++ refugio_app/apps/mascota/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 
-from apps.mascota.views import index, mascota_view, mascota_list, mascota_edit, mascota_delete
+from apps.mascota.views import index, mascota_view, mascota_list, mascota_edit, mascota_delete, MascotaList, MascotaUpdate, MascotaDelete
 
 app_name = 'mascotas'
 urlpatterns = [
     path('', index, name='index'),
+    path('listado/', MascotaList.as_view(), name='mascota_listar'),
     path('nuevo/', mascota_view, name='mascota_crear'),
-    path('listado/', mascota_list, name='mascota_listar'),
-    path('editar/<id_mascota>/', mascota_edit, name='mascota_editar'),
-    path('eliminar/<id_mascota>/', mascota_delete, name='mascota_eliminar'),
+    path('editar/<pk>/', MascotaUpdate.as_view(), name='mascota_editar'),
+    path('eliminar/<pk>/', MascotaDelete.as_view(), name='mascota_eliminar'),
 ]
